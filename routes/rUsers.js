8c0b475616264fad7a3ab4fe88cf162ff22ad9c0
liuslin28 <liuslin28@163.com>
2017-12-01 16:39:30 +0800

    OrderSuccess test1

--- routes/rUsers.js
+++ routes/rUsers.js
@@ -345,4 +345,51 @@ router.post("/payMent",function (req,res,next) {
     });
 });
 
+// 根据订单Id查询订单信息
+router.get("/orderDetail",function (req,res,next) {
+   let userId = req.cookies.userId;
+   let orderId = req.body.orderId;
+   User.findOne({userId:userId},function (err,userInfo) {
+       if(err) {
+           res.json({
+               suatus: '1',
+               msg:err.message,
+               result: ''
+           });
+       } else {
+           let  orderList = userInfo.orderList;
+           if(orderList.length > 0) {
+               let orderTotal = 0;
+               orderList.forEach((item) => {
+                  if(item.orderId === orderId) {
+                      orderTotal =item.orderTotal;
+                   }
+               });
+               if(orderTotal>0) {
+                   res.json({
+                       status: '0',
+                       msg:'',
+                       result: {
+                           orderId: orderId,
+                           orderTotal: orderTotal
+                       }
+                   });
+               } else {
+                   res.json({
+                       status: '120002',
+                       msg:'无此订单',
+                       result:''
+                   });
+               }
+           } else {
+           res.json({
+               status: '120001',
+               msg:'当前用户未创建订单',
+               result:''
+           });
+           }
+       }
+   })
+});
+
 module.exports = router;
